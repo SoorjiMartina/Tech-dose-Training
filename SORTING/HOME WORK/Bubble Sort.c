9a3f8f2cad44cc5a44be95f22a294fa6857f4982
SoorjiMartina <88356951+SoorjiMartina@users.noreply.github.com>
2022-09-08 13:56:04 +0530

    Create Bubble Sort.c

---
+++ SORTING/HOME WORK/Bubble Sort.c
@@ -0,0 +1,33 @@
+#include<stdbool.h>
+int main(void) {
+  int n;
+  scanf("%d",&n);
+  int arr[n];
+  for(int i=0;i<n;i++)
+    {
+      scanf("%d",arr+i);
+    }
+  for(int i=n-1;i>=0;i++)
+    {
+      bool issawpped= false;
+      for(int j=0;j<n;j++)
+        {
+          if(arr[j]>arr[j+1])
+          {
+            int temp =arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+            issawpped = true;
+          }
+        }
+      if(issawpped==false)
+      {
+        break;
+      }
+    }
+  for(int i=0;i<n;i++)
+    {
+      printf("%d ",arr[i]);
+    }
+  return 0;
+}

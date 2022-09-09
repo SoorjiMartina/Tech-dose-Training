970062ea66cd76030bd5361bb9e9df14f91cfaca
SoorjiMartina <88356951+SoorjiMartina@users.noreply.github.com>
2022-09-09 12:37:49 +0530

    Update lcm.c

--- BEGINNER ASSIGNMENT/lcm.c
+++ BEGINNER ASSIGNMENT/lcm.c
@@ -14,3 +14,16 @@ int main(void) {
     }
   return 0;
 }
+Method:2
+#include <stdio.h>
+#define ULL unsigned long long int
+ULL gcd(ULL a,ULL b)
+{
+  return (a==0) ? b : gcd(b%a,a);
+}
+int main(void) {
+ ULL a,b;
+  scanf("%lld%lld",&a,&b);
+  printf("%lld",(a*b)/gcd(b,a));
+  return 0;
+}

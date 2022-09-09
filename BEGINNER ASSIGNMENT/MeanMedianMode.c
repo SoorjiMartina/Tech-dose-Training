9a834b48566719814950a06ba29c2d5446c96e18
SoorjiMartina <88356951+SoorjiMartina@users.noreply.github.com>
2022-09-09 14:58:15 +0530

    Create MeanMedianMode.c

---
+++ BEGINNER ASSIGNMENT/MeanMedianMode.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(void) {
+  int n;
+  scanf("%d",&n);
+  int arr[n];
+  for(int i=0;i<n;i++)
+    {
+      scanf("%d",arr+i);
+    }
+  //mean
+  int mean=0;
+  for(int i=0;i<n;i++)
+    {
+      mean+=arr[i];
+    }
+  printf("%d\n",mean/n);
+  //median
+  if(n%2!=0)
+  {
+      printf("%d\n",(n+1)/2);
+  }
+  else
+  {
+    printf("%.2f\n",((n)/2+((n/2)+1))/2.0);
+  }
+  //mode
+  int maxcount=0,count=0,maxval=0;
+  for(int i=0;i<n;i++){
+    count=0;
+    for(int j=0;j<n;j++)
+      {
+         if(arr[i]==arr[j])
+         {
+           count++;
+         }
+      }
+  if(count>maxcount)
+  {
+    maxcount=count;
+    maxval=arr[i];
+  }
+  }
+  printf("%d %d\n",maxval,maxcount);
+  return 0;
+}

b1487c6f0c68c8f8c5e689448ec5dd04287cfd84
SoorjiMartina <88356951+SoorjiMartina@users.noreply.github.com>
2022-10-13 13:59:12 +0530

    Update weekly contest 239.java

--- LEETCODE CONTEST/weekly contest 239.java
+++ LEETCODE CONTEST/weekly contest 239.java
@@ -1,10 +1,16 @@
 class Solution {
     public int getMinDistance(int[] nums, int target, int start) {
-        int min =101;
-        for(int i=start;i<nums.length;i++){
+       int n = nums.length;
+        for(int i=0;i<nums.length;i++)
+        {
             if(nums[i]==target)
-              min=Math.min(Math.abs(i-start),min);
+            {
+                if(Math.abs(i-start)<n)
+                {
+                    n = Math.abs(i-start);
+                }
+            }
         }
-        return min;
+        return n;
     }
 }

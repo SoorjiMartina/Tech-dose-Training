96a0b8b286feecb4d9aa7f82b489840541cb4da1
SoorjiMartina <88356951+SoorjiMartina@users.noreply.github.com>
2022-10-13 14:17:17 +0530

    Create weekly contest 277.java

---
+++ LEETCODE CONTEST/weekly contest 277.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countElements(int[] nums) {
+       int min = nums[0];
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]<min)
+            {
+                min=nums[i];
+            }
+                
+        }
+        int max= nums[0];
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>max)
+            {
+                max= nums[i];
+            }
+        }
+        int count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>min && nums[i]<max)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}

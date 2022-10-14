0943f6cce555bdcd2910c20580a3689fe1d1898e
SoorjiMartina <88356951+SoorjiMartina@users.noreply.github.com>
2022-10-14 14:08:27 +0530

    Update biweekly contest 56.java

--- LEETCODE CONTEST/biweekly contest 56.java
+++ LEETCODE CONTEST/biweekly contest 56.java
@@ -1,14 +1,17 @@
 class Solution {
     public int countTriples(int n) {
         int count=0;
-        for(int i=1;i<=n;i++)
+        for(int i=1;i<n;i++)
         {
-            for(int j=i+1;j<=n;j++)
+            for(int j=1;j<n;j++)
             {
-                double c = Math.sqrt((i*i)+(j*j));
-                if(c==(int) c && c<=n)
+                for(int k=1;k<=n;k++)
                 {
-                    count+=2;
+                    if((i*i )+ (j*j)==(k*k))
+                        
+                    {
+                        count++;
+                    }
                 }
 
             }

004ad4ef3eb7bad5d3b3e881fff05e2ef15114ad
SoorjiMartina <88356951+SoorjiMartina@users.noreply.github.com>
2022-10-14 14:38:23 +0530

    Create weekly contest 275.java

---
+++ LEETCODE CONTEST/weekly contest 275.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkValid(int[][] matrix) {
+     int n = matrix.length;
+        for(int i=0;i<n;i++)
+        {
+            Set<Integer> row = new HashSet<>();
+            Set<Integer> col = new HashSet<>();
+            for(int j=0;j<n;j++)
+            {
+                row.add(matrix[i][j]);
+                col.add(matrix[j][i]);
+            }
+            if(row.size() <n || col.size() < n)
+            {
+                return false;
+            }
+        }
+    
+        return true;
+    }
+}

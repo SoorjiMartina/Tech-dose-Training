789026c0bb1f64b9fd3b9bb16214ddf639144021
SoorjiMartina <88356951+SoorjiMartina@users.noreply.github.com>
2022-10-16 17:39:49 +0530

    Create Reverse Linked List.c

---
+++ LINKED LIST/ASSIGNMENT/Reverse Linked List.c
@@ -0,0 +1,11 @@
+struct ListNode* reverseList(struct ListNode* head){
+    struct ListNode * prev = NULL;
+    while(head)
+    {
+        struct Listnode * temp = head->next;
+        head->next = prev;
+        prev= head;
+        head= temp;
+    }
+    return prev;
+}

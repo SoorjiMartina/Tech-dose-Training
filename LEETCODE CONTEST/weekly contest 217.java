56ae47c0e6a19e7ab36497158d6ea9c9d75ecc76
SoorjiMartina <88356951+SoorjiMartina@users.noreply.github.com>
2022-10-18 11:02:35 +0530

    Create weekly contest 217.java

---
+++ LEETCODE CONTEST/weekly contest 217.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxwealth=0;
+        for(int i=0;i<accounts.length;i++)
+        {
+            int sum=0;
+            for(int j=0;j<accounts[0].length;j++)
+            {
+                sum+=accounts[i][j];
+            }
+            maxwealth=Math.max(sum,maxwealth);
+        }
+        return maxwealth;
+        
+    }
+}

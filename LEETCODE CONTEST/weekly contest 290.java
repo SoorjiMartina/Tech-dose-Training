fa8593c4eec0a06b3f88421dbe54c45ec8eaed96
SoorjiMartina <88356951+SoorjiMartina@users.noreply.github.com>
2022-10-19 12:33:25 +0530

    Create weekly contest 290.java

---
+++ LEETCODE CONTEST/weekly contest 290.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> intersection(int[][] nums) {
+        List<Integer> list = new ArrayList<>();
+        int count [] = new int [1001];
+        for(int []  arr : nums)
+        {
+            for(int i :arr)
+            {
+                count[i]++;
+            }
+        }
+        for(int i=0;i<count.length;i++)
+        {
+            if(count[i]== nums.length)
+            {
+                list.add(i);
+            }
+        }
+        
+        return list;
+    }
+}

eee8f7962d8a61488846d842ced89708d114a509
SoorjiMartina <88356951+SoorjiMartina@users.noreply.github.com>
2022-10-20 10:10:44 +0530

    Create biweekly contest 77.java

---
+++ LEETCODE CONTEST/biweekly contest 77.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countPrefixes(String[] words, String s) {
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            String str  = s.substring(0,i+1);
+            for(int j=0;j<words.length;j++)
+            {
+                if(str.equals(words[j]))
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}

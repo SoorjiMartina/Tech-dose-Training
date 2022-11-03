63bb1fb88cee727f9bb63e33382b356b7b50b40d
SoorjiMartina <88356951+SoorjiMartina@users.noreply.github.com>
2022-11-03 14:53:47 +0530

    Create Kth Smallest Element in a Sorted Matrix.java

---
+++ MATRIX/ASSIGNMENT/Kth Smallest Element in a Sorted Matrix.java
@@ -0,0 +1,18 @@
+ public int kthSmallest(int[][] matrix, int k) {
+         PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[0].length;j++)
+            {
+                pq.add(matrix[i][j]);
+    
+            }
+        }
+        for(int i=1;i<k;i++){
+        pq.poll();
+        }
+        return pq.poll();
+        
+        
+    }
+}

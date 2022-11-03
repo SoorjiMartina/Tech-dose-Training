3cf8db2256a86018f46066d29edea7704f1340c0
SoorjiMartina <88356951+SoorjiMartina@users.noreply.github.com>
2022-11-03 14:02:07 +0530

    Create Find Median from Data Stream.java

---
+++ HEAP/ASSIGNMENT/Find Median from Data Stream.java
@@ -0,0 +1,30 @@
+class MedianFinder {
+    PriorityQueue<Integer> MinHeap;
+    PriorityQueue<Integer> MaxHeap;
+
+    public MedianFinder() {
+     MaxHeap  = new PriorityQueue(Collections.reverseOrder());
+     MinHeap  = new PriorityQueue();
+        
+    }
+    
+    public void addNum(int num) {
+        MaxHeap.offer(num);
+        MinHeap.offer(MaxHeap.poll());
+        if(MinHeap.size()> MaxHeap.size())
+        {
+            MaxHeap.offer(MinHeap.poll());
+        }
+        
+       
+    }
+    
+    public double findMedian() {
+        if(MaxHeap.size() == MinHeap.size())
+            return (MaxHeap.peek() + MinHeap.peek())/2.0;
+        else
+            return MaxHeap.peek();
+     
+        
+    }
+}

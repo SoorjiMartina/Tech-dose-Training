d0f0fa2589dcf0c4c6dd93fce641714a7de984e5
SoorjiMartina <88356951+SoorjiMartina@users.noreply.github.com>
2022-12-28 19:01:20 +0530

    Create Super Egg Drop.java

---
+++ DP/ASSIGNMENT/Super Egg Drop.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int superEggDrop(int k, int n) {
+        int [] [] dp = new int [n+1] [k+1];
+        int m=0;
+        while(dp[m][k]<n){
+            ++m;
+        for(int i=1;i<=k;i++)
+            dp[m][i]=dp[m-1][i-1]+dp[m-1][i]+1;
+        }
+       return m; 
+    }
+}
